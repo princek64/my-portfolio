3b4e030d7cb7b0a8947df21e529ed014b49f1921
prince.kukreja <prince.kukreja@sparkbusinessworks.com>
2024-12-15 17:48:02 +0530

    feat: update bio

--- app/page.tsx
+++ app/page.tsx
@@ -18,21 +18,20 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-xl font-medium tracking-tight">
-        Full-Stack Developer | Architect | Problem Solver
+        Full-Stack Developer | Crafting Web & Mobile Apps
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          I’m a Full-Stack Developer passionate about building web applications that are both user-friendly and powerful.
-          With experience in both frontend and backend development, I enjoy tackling complex challenges and creating solutions that help businesses grow.
+          I’m a Full-Stack Developer with a strong focus on frontend development, passionate about building seamless web and mobile experiences that combine functionality with user-centered design.
         </p>
         <p>
-          I specialize in building scalable applications that solve real problems, using modern technologies like Next.js, React, Vue, TypeScript, and Express.
-          I’m also experienced with mobile development using Ionic.
+          I specialize in building scalable applications that solve real problems, using modern technologies like Next.js, React, Vue, and Nuxt.
+          While frontend development is my forte, I also bring experience in backend and mobile development with Ionic.
         </p>
         <p>
-          I’m always eager to learn new skills and stay up-to-date with the latest tech trends. Outside of coding, you can find me enjoying a good cup of coffee ☕, exploring new places 🌍, or hitting the slopes 🏂.
-          I’m also deeply committed to lifelong learning and sharing my knowledge with others to help build better, more inclusive tech communities.
+          I’m always eager to learn new skills and stay up-to-date with the latest tech trends. Outside of coding, you can find me enjoying a good cup of coffee ☕, exploring new places 🌍, hitting the slopes 🏂 or getting my hands into some pottery 🏺.
+          I’m passionate about learning, sharing knowledge, and supporting inclusive tech communities through open-source contributions.
           Take a look at my work, and let’s connect to discuss how we can build something amazing together!
         </p>
         <p className="flex items-center gap-2"><FiLink /><a href="https://drive.google.com/file/d/1my94ugCJbmZCfu16QRr9sGRndaeQ2ybJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume Link</a></p>
